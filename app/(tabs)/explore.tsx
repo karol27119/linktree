e8ec2c3631d8795c0aff322f3a476a321ddcbefc
karol27119 <116905552+karol27119@users.noreply.github.com>
2024-06-12 13:59:51 +0000

    feat commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,5 +1,5 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, Linking, TouchableOpacity, Text } from 'react-native';
 
 import { useCallback } from 'react';
 import { Collapsible } from '@/components/Collapsible';
@@ -28,6 +28,15 @@ export default function TabTwoScreen() {
     return null;
   }
 
+  const InstagramLink = async () => {
+    const instagramUrl = "instagram://user?username=karolpelegrini";
+    try {
+      await Linking.openURL(instagramUrl);
+    } catch (error) {
+      console.error("Erro ao abrir o link do Instagram:", error);
+    }
+  };
+  
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#F0F1F0', dark: '#F0F1F0' }}
@@ -45,74 +54,9 @@ export default function TabTwoScreen() {
       <ThemedView style={styles.titleContainer}>
       </ThemedView>
       <ThemedText style={styles.text}>Estudante | Técnico em Informática | Ajudante em Logistíca</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+      <TouchableOpacity style={styles.botao} onPress={InstagramLink}>
+        <Text>Ir para o Instagram</Text>
+      </TouchableOpacity>
     </ParallaxScrollView>
   );
 }
@@ -129,9 +73,9 @@ const styles = StyleSheet.create({
     gap: 8,
   },
   reactLogo: {
-    height: 230,
+    height: 250,
     width: 380,
-    bottom: 0,
+    bottom: 2,
     left: 5,
     position: 'absolute',
   },
@@ -146,5 +90,16 @@ const styles = StyleSheet.create({
     fontFamily:"fonte1",
     color:"#48392E",
     fontSize:16,
-  }
+  },
+  botao: {
+    flexDirection: "row",
+    backgroundColor: "#ffff",
+    borderColor: "#ffff",
+    height: 50,
+    width: 250,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf:"center",
+    borderRadius: 100,
+  },
 });
